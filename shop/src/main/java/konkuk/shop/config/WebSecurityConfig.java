fa70704f01d19900b24bfcc013fec65bd1585ab3
unknown <hsk4991149@naver.com>
2022-02-06 17:20:03 +0900

    config cors

--- shop/src/main/java/konkuk/shop/config/WebSecurityConfig.java
+++ shop/src/main/java/konkuk/shop/config/WebSecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -23,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // http 시큐리티 빌더
-        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정
+        http.cors().configurationSource(corsConfigurationSource()) // cors 설정파일 등록
                 .and()
                 .csrf().disable() // csrf는 현재 사용하지 않으므로 disable
                 .httpBasic().disable() // token을 사용하므로 basic 인증 disable
@@ -43,6 +46,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 jwtAuthenticationFilter,
                 CorsFilter.class
         );
+    }
+
+    // CORS 허용 설정 Bean
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
