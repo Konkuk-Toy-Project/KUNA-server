23d5edab3ccf5efd403401fd5d050ab5115b62d4
seungtaekhong <hsk4991149@naver.com>
2022-02-11 23:47:23 +0900

    update cart service: add item in cart

--- shop/src/main/java/konkuk/shop/controller/CartController.java
+++ shop/src/main/java/konkuk/shop/controller/CartController.java
@@ -3,6 +3,7 @@ package konkuk.shop.controller;
 import konkuk.shop.dto.CartItemDto;
 import konkuk.shop.form.requestForm.cart.RequestAddItemInCartForm;
 import konkuk.shop.form.requestForm.cart.RequestChangeCountForm;
+import konkuk.shop.form.responseForm.cart.AddItemInCartResponseForm;
 import konkuk.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,12 +24,14 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping
-    public HashMap<String, Object> addItemInCart(@AuthenticationPrincipal Long userId,
-                                 @RequestBody RequestAddItemInCartForm form) {
-        Long cartItemId = cartService.addItem(userId, form.getItemId(), form.getOption1Id(), form.getOption2Id(), form.getCount());
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("cartItemId", cartItemId);
-        return result;
+    public ResponseEntity<List<AddItemInCartResponseForm>> addItemInCart(@AuthenticationPrincipal Long userId,
+                                                                         @RequestBody List<RequestAddItemInCartForm> forms) {
+        List<AddItemInCartResponseForm> result = new ArrayList<>();
+        for(RequestAddItemInCartForm form : forms) {
+            Long cartItemId = cartService.addItem(userId, form.getItemId(), form.getOption1Id(), form.getOption2Id(), form.getCount());
+            result.add(new AddItemInCartResponseForm(cartItemId));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @DeleteMapping("/{cartItemId}")
---
+++ shop/src/main/java/konkuk/shop/form/responseForm/cart/AddItemInCartResponseForm.java
@@ -0,0 +1,10 @@
+package konkuk.shop.form.responseForm.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddItemInCartResponseForm {
+    Long cartItemId;
+}
